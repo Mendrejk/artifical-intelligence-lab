c454e20b94011452e9ab05c32658a5417bbb62ce
Mendrejk <bastian.lakomy@gmail.com>
2022-03-08 17:12:30 +0100

    Fix types (int -> i64), derive Debug for both structs

--- src/facility_layout.rs
+++ src/facility_layout.rs
@@ -1,10 +1,12 @@
-struct FacilityLayout {
-    facility_flows: Vec<FacilityFlow>
+#[derive(Debug)]
+pub struct FacilityLayout {
+    pub facility_flows: Vec<FacilityFlow>
 }
 
-struct FacilityFlow {
-    source: int,
-    dest: int,
-    amount: int,
-    cost: int
+#[derive(Debug)]
+pub struct FacilityFlow {
+    pub source: i64,
+    pub dest: i64,
+    pub amount: i64,
+    pub cost: i64
 }
