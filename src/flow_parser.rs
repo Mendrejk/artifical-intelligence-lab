e7da73c1d7842465b7b31e0e6f891f37eb1c09af
Mendrejk <bastian.lakomy@gmail.com>
2022-03-08 17:13:41 +0100

    Add methods to parse JSON flows to a FacilityLayout

---
+++ src/flow_parser.rs
@@ -0,0 +1,36 @@
+use serde_json::{Result, Value};
+
+use crate::facility_layout::{FacilityFlow, FacilityLayout};
+
+use std::env;
+use std::fs;
+
+pub fn parse_flows(flow_file_path: &str, cost_file_path: &str) -> FacilityLayout {
+    // read the JSON files into strings
+    let flow_contents = fs::read_to_string(flow_file_path).unwrap();
+    let cost_contents = fs::read_to_string(cost_file_path).unwrap();
+
+    // parse the JSONs
+    let flows: Vec<Value> = serde_json::from_str(&flow_contents).unwrap();
+    let costs: Vec<Value> = serde_json::from_str(&cost_contents).unwrap();
+
+    // map the JSONs to FacilityFlows
+    // TODO use map() instead of doing this imperatively, somehow?
+    let mut facility_flows: Vec<FacilityFlow> = Vec::new();
+
+    for i in flows.iter().zip(costs.iter()) {
+        let (flow, cost) = i;
+        facility_flows.push(parse_facility_flow(flow, cost));
+    }
+
+    FacilityLayout { facility_flows }
+}
+
+fn parse_facility_flow(flow: &Value, cost: &Value) -> FacilityFlow {
+    FacilityFlow {
+        source: flow["source"].as_i64().unwrap(),
+        dest: flow["dest"].as_i64().unwrap(),
+        amount: flow["amount"].as_i64().unwrap(),
+        cost: cost["cost"].as_i64().unwrap()
+    }
+}
